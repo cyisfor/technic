996b629586b3c8197883872018b0a2f78193cfd3
Cy <whatever>
2013-10-06 18:48:40 -0700

    Drops can't be a scoped variable
    Putting get_node_drops in override_node_drops is too many assumptions
    IMO.

--- technic/helpers.lua
+++ technic/helpers.lua
@@ -12,15 +12,14 @@ end
 function nothing(...) end
 
 -- it's safe for machines to use minetest.node_dig here w/out drops happening
-function technic.override_node_drops(nodename, toolname, handler)
-    local drops = minetest.get_node_drops(nodename, toolname)
+function technic.override_node_drops(handler)
     if minetest.handle_node_drops == nothing then
-        handle(drops)
+        handler()
     else
         local disabled_node_drops = minetest.handle_node_drops
         minetest.handle_node_drops = nothing
         -- pcall here? nah, let errors just kill everything. #theluaway
-        handler(drops)
+        handler()
         minetest.handle_node_drops = disabled_node_drops
     end
 end
